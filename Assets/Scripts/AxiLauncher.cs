5005c75b3dc79f3119a33f50ef2ee266ab924c45
sin365 <353374337@qq.com>
2025-06-04 23:44:11 +0800

    追加Launcher流程

---
+++ Assets/Scripts/AxiLauncher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AxiLauncher : MonoBehaviour
+{
+
+	private void OnEnable()
+	{
+		InitGame();
+	}
+
+	void InitGame()
+	{
+		GameObject.DontDestroyOnLoad(gameObject);
+#if UNITY_SWITCH
+		AxiNS.instance.Init();
+#endif
+		SceneManager.LoadScene("Title");
+		//global::UnityEngine.Application.LoadLevel("Title");
+	}
+}
