61f5d2305c642dc8c5940da65d1cd1b6a0770c91
sin365 <353374337@qq.com>
2025-06-08 23:01:44 +0800

    修改IO到多平台的胶水代码

--- Assets/Scripts/AxiIO/NSwitchIO.cs
+++ Assets/Scripts/AxiIO/NSwitchIO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace AxiIO
 {
@@ -7,30 +8,50 @@ namespace AxiIO
 	{
 		public NSwitchIO()
 		{
+			Debug.Log($"NSwitchIO Init");
+			//return;
 			AxiNS.instance.Init();
+		}
+
+		public void Ping()
+		{
+			throw new NotImplementedException();
 		}
 		public void dir_CreateDirectory(string dirpath)
 		{
+			//Debug.Log($"dir_CreateDirectory");
+			//return;
 			AxiNS.instance.io.CreateDir(dirpath);
 		}
 
 		public void dir_Delete(string path, bool recursive)
 		{
-			AxiNS.instance.io.DeletePathFile(path);
+			//Debug.Log($"dir_Delete recursive:{recursive}");
+			//return;
+			if (recursive)
+				AxiNS.instance.io.DeletePathDirRecursively(path);
+			else
+				AxiNS.instance.io.DeletePathDir(path);
 		}
 
 		public IEnumerable<string> dir_EnumerateFiles(string path, string searchPattern)
 		{
+			//Debug.Log($"dir_EnumerateFiles path=>{path} searchPattern=>{searchPattern}");
+			//return default;
 			return AxiNS.instance.io.EnumerateFiles(path, searchPattern);
 		}
 
 		public bool dir_Exists(string dirpath)
 		{
+			//Debug.Log($"dir_Exists path=>{dirpath}");
+			//return default;
 			return AxiNS.instance.io.CheckPathExists(dirpath);
 		}
 
 		public string[] dir_GetDirectories(string path)
 		{
+			//Debug.Log($"dir_GetDirectories path=>{path}");
+			//return default;
 			if (!AxiNS.instance.io.GetDirectoryDirs(path, out string[] result))
 			{
 				return new string[0];
@@ -40,6 +61,8 @@ namespace AxiIO
 
 		public string[] dir_GetFiles(string path)
 		{
+			//Debug.Log($"dir_GetFiles path=>{path}");
+			//return default;
 			if (!AxiNS.instance.io.GetDirectoryFiles(path, out string[] result))
 			{
 				return new string[0];
@@ -49,21 +72,31 @@ namespace AxiIO
 
 		public void file_Delete(string filePath)
 		{
+			//Debug.Log($"file_Delete path=>{filePath}");
+			//return;
 			AxiNS.instance.io.DeletePathFile(filePath);
 		}
 
 		public bool file_Exists(string filePath)
 		{
-			return AxiNS.instance.io.CheckPathExists(filePath);
+			//Debug.Log($"file_Exists path=>{filePath}");
+			//return default;
+			bool result = AxiNS.instance.io.CheckPathExists(filePath);
+			//Debug.Log($"file_Exists path=>{filePath} result=>{result}");
+			return result;
 		}
 
 		public byte[] file_ReadAllBytes(string filePath)
 		{
+			//Debug.Log($"file_ReadAllBytes path=>{filePath}");
+			//return default;
 			return AxiNS.instance.io.LoadSwitchDataFile(filePath);
 		}
 
 		public int file_ReadBytesToArr(string filePath, byte[] readToArr, int start, int len)
 		{
+			//Debug.Log($"file_ReadBytesToArr filePath=>{filePath},readToArr.Length=>{readToArr.Length},start=>{start},len=>{len}");
+			//return default;
 			byte[] bytes = file_ReadAllBytes(filePath);
 			int templen = Math.Min(len, bytes.Length);
 			Array.Copy(readToArr, readToArr, len);
@@ -78,12 +111,17 @@ namespace AxiIO
 		/// <param name="immediatelyCommit">是否立即Commit到物理存储</param>
 		public void file_WriteAllBytes(string filePath, byte[] data, bool immediatelyCommit = true)
 		{
+			//Debug.Log($"file_WriteAllBytes filePath=>{filePath},data.Length=>{data.Length},immediatelyCommit=>{immediatelyCommit}");
+			//return;
 			AxiNS.instance.io.FileToSaveWithCreate(filePath, data, immediatelyCommit);
 		}
 
 		public void file_WriteAllBytes(string filePath, System.IO.MemoryStream ms)
 		{
+			//Debug.Log($"file_WriteAllBytes filePath=>{filePath},ms.Length=>{ms.Length}");
+			//return;
 			AxiNS.instance.io.FileToSaveWithCreate(filePath, ms);
 		}
+
 	}
 }
